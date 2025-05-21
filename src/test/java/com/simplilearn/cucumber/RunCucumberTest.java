c4970d61b38e02769a12fe8ae43d7d885c620526
David Serrano <serranoadavid@gmail.com>
2025-05-21 16:30:00 -0700

    add new feature xxxx

--- src/test/java/com/simplilearn/cucumber/RunCucumberTest.java
+++ src/test/java/com/simplilearn/cucumber/RunCucumberTest.java
@@ -14,4 +14,5 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.simplilearn.cucumber")
 public class RunCucumberTest {
+	// Test suite runner for cucumber 
 }
